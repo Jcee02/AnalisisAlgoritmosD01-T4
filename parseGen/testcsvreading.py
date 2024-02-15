2a39f295038e0b279aaf0113ab3c05f1b1b08ccd
Jcee02 <118766109+Jcee02@users.noreply.github.com>
2024-02-15 16:03:31 -0600

    Update testcsvreading.py
    only chunks

--- parseGen/testcsvreading.py
+++ parseGen/testcsvreading.py
@@ -1,40 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  9 13:54:20 2024
-
-@author: jc
-"""
-
 import pandas as pd
 
 gd = pd.read_csv("data/dataset.csv")
 
 
-#Función para reemplazar caracteres en la cadena
+# Función para reemplazar caracteres en la cadena
 def reemplazar_caracteres(string, posicion, alteracion):
-    return string[:posicion-1] + alteracion + string[posicion:]
+    return string[:posicion - 1] + alteracion + string[posicion:]
+
 
 # Lista de las combinaciones modificadas
 combinaciones_modificadas = []
 
-#recorrer las filas del .csv
+# recorrer las filas del .csv
 for index, row in gd.iterrows():
     referencia = str(row['referencia'])
     alteracion = str(row['alteracion'])
     string_a_modificar = str(row["string_a_modificar"])
-    
-    #recorrer cada ventana de 10 caracteres
-    for i in range(0, len(string_a_modificar)-9,5):
-        ventana = string_a_modificar[i:i+10]
-        
-        #Reemplazar caracter en la posicion especificada
+
+    # recorrer cada ventana de 10 caracteres
+    for i in range(0, len(string_a_modificar) - 9, 5):
+        ventana = string_a_modificar[i:i + 10]
+
+        # Reemplazar caracter en la posicion especificada
         ventana_modificada = reemplazar_caracteres(ventana, int(row['posicion']), alteracion)
         combinaciones_modificadas.append(ventana_modificada)
-        
-        
-for combinaciones in combinaciones_modificadas:
-    print(combinaciones_modificadas)
 
+df = pd.DataFrame(combinaciones_modificadas)
 
-
+df.to_csv("result.csv")
