cf5f7de687986d4a395b8aa52461d6851d1100f1
Jcee02 <118766109+Jcee02@users.noreply.github.com>
2024-02-15 16:51:10 -0600

    Clase testcsvreading.py

--- parseGen/testcsvreading.py
+++ parseGen/testcsvreading.py
@@ -1,30 +1,32 @@
 import pandas as pd
 
-gd = pd.read_csv("data/dataset.csv")
+class Cadena:
+    def __init__(self, csv_file):
+        self.df = pd.read_csv(csv_file)
+        self.chunks = []
 
+    def dividir_cadena(self):
+        # Eliminar las filas con valores NaN en la columna "string_a_modificar"
+        self.df.dropna(subset=['string_a_modificar'], inplace=True)
 
-# Función para reemplazar caracteres en la cadena
-def reemplazar_caracteres(string, posicion, alteracion):
-    return string[:posicion - 1] + alteracion + string[posicion:]
+        # Iterar sobre las filas restantes para dividir las cadenas
+        for _, row in self.df.iterrows():
+            string_a_dividir = str(row["string_a_modificar"])
 
+            for i in range(0, len(string_a_dividir) - 9, 5):
+                chunk = string_a_dividir[i:i + 10]
+                self.chunks.append(chunk)
 
-# Lista de las combinaciones modificadas
-combinaciones_modificadas = []
+            # Verificar si hay un chunk restante al final
+            if len(string_a_dividir) % 5 != 0:
+                chunk = string_a_dividir[-(len(string_a_dividir) % 5):]
+                self.chunks.append(chunk)
 
-# recorrer las filas del .csv
-for index, row in gd.iterrows():
-    referencia = str(row['referencia'])
-    alteracion = str(row['alteracion'])
-    string_a_modificar = str(row["string_a_modificar"])
+    def guardar_resultados(self, output_file):
+        df_resultado = pd.DataFrame(self.chunks, columns=['chunk'])
+        df_resultado.to_csv(output_file, index=False)
 
-    # recorrer cada ventana de 10 caracteres
-    for i in range(0, len(string_a_modificar) - 9, 5):
-        ventana = string_a_modificar[i:i + 10]
-
-        # Reemplazar caracter en la posicion especificada
-        ventana_modificada = reemplazar_caracteres(ventana, int(row['posicion']), alteracion)
-        combinaciones_modificadas.append(ventana_modificada)
-
-df = pd.DataFrame(combinaciones_modificadas)
-
-df.to_csv("result.csv")
+# Uso de la clase
+divisor = Cadena("data/dataset.csv")
+divisor.dividir_cadena()
+divisor.guardar_resultados("result.csv")
