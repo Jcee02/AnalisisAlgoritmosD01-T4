f4b3dc2a07462669ad50d2dafdb4ee82c261f235
Jcee02 <118766109+Jcee02@users.noreply.github.com>
2024-02-16 08:27:12 -0600

    Me ando haciendo bolas XD
    #TO:DO Equipar con los métodos necesarios a la clase para que una sola instancia de ella pueda ser usada para correr todo el programa.
    # - Método para crear todas las variantes de los 8 cambios iniciales, resultado final 8!
    # - Método para crear las variantes de un par de chunks dados (la validación del primero > segundo la hace el Front)
    # - Método para crear las variantes de un rango de chunks dados (la validación de que el rango sea válido la hace el f

--- parseGen/testcsvreading.py
+++ parseGen/testcsvreading.py
@@ -1,13 +1,28 @@
 import pandas as pd
 
-class Cadena:
-    def __init__(self, csv_file):
-        self.df = pd.read_csv(csv_file)
-        self.chunks = []
+#TO:DO Equipar con los métodos necesarios a la clase para que una sola instancia de ella pueda ser usada para correr todo el programa.
+# - Método para crear todas las variantes de los 8 cambios iniciales, resultado final 8!
+# - Método para crear las variantes de un par de chunks dados (la validación del primero > segundo la hace el Front)
+# - Método para crear las variantes de un rango de chunks dados (la validación de que el rango sea válido la hace el front)
 
-    def dividir_cadena(self):
-        # Eliminar las filas con valores NaN en la columna "string_a_modificar"
+
+class Programa:
+
+    chunks = []
+    
+    def __init__(self, csv_file):
+        #inicializar DataFrame para cada instania de Cadena
+        self.df = pd.read_csv(csv_file)
+        #Eliminar los fakin valores NaN de la columna string_a_modificar
         self.df.dropna(subset=['string_a_modificar'], inplace=True)
+        #Lista de diccionarios con cada fila del dataframe
+        self.dic_df = df.to_dic(orient='records')
+        
+        
+
+    def 
+    
+    def dividir_cadena(self):        
 
         # Iterar sobre las filas restantes para dividir las cadenas
         for _, row in self.df.iterrows():
@@ -26,7 +41,7 @@ class Cadena:
         df_resultado = pd.DataFrame(self.chunks, columns=['chunk'])
         df_resultado.to_csv(output_file, index=False)
 
-# Uso de la clase
-divisor = Cadena("data/dataset.csv")
+# Instancia de prueba???????????????????????????????
+divisor = Programa("data/dataset.csv")
 divisor.dividir_cadena()
 divisor.guardar_resultados("result.csv")
